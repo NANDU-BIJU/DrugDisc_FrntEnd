ee2a05fb07607ad353793d512f8caf6cee419a32
Achyuth Mohan <achyuthmohan1@gmail.com>
2024-03-19 00:50:17 +0530

    css

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
-
+import './MainPage.css'
 const MainPage = () => {
   return (
-    <div>
-      <h1>Drug Discovery Frontend</h1>
+    <div classname="landing__pg_main">
+      <h1>Drug Discovery </h1>
+      <div className="drugs__cards__display">
+        <form action="">
+            <input type="text" />
+            <button>Submit</button>
+        </form>
+      </div>
     </div>
   )
 }
