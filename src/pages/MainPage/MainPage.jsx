2e66a96a1be88fb76e9d8f9a8e7e9852c15cbf14
Achyuth Mohan <achyuthmohan1@gmail.com>
2024-03-21 12:53:44 +0530

    css

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,20 +1,49 @@
-import React from 'react'
-import './MainPage.css'
+import React, { useState } from "react";
+import "./MainPage.css";
+import axios from "axios";
+import { baseUrl } from "../../utils/urls";
 const MainPage = () => {
+  const [smileProtein, setProtein] = useState("");
+  const [smilesDrug, setSmilesDrug] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios
+      .post(`${baseUrl}/generate-alternate-drug/`,{
+        smiles_notation_protein: smileProtein,
+        smiles_notation_drug: smilesDrug,
+      })
+      .then((response) => {
+        console.log(response.data);
+      },(error)=>{
+        console.log(error)
+      });
+  };
   return (
     <div classname="landing__pg_main">
       <h1>Drug Discovery </h1>
       <div className="drugs__cards__display">
-        <form action="">
-        <label>Smiles Notation for protein</label>
-            <input type="text" />
-            <label>Smiles notation of inhibitor</label>
-            <input type="text" />            
-            <button>Submit</button>
+        <form onSubmit={handleSubmit}>
+          <label>Smiles Notation for protein</label>
+          <input
+            value={smileProtein}
+            onChange={(e) => {
+              setProtein(e.target.value);
+            }}
+            type="text"
+          />
+          <label>Smiles notation of inhibitor</label>
+          <input
+            value={smilesDrug}
+            onChange={(e) => {
+              setSmilesDrug(e.target.value);
+            }}
+            type="text"
+          />
+          <button type='submit'>Submit</button>
         </form>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default MainPage
+export default MainPage;
